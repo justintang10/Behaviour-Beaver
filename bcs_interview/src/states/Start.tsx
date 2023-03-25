22872ee34155b42dabf57962388400e29077acd0
helenzhouu <74159917+helenzhouu@users.noreply.github.com>
2023-03-25 13:01:06 -0700

    updated start.tsx

--- bcs_interview/src/states/Start.tsx
+++ bcs_interview/src/states/Start.tsx
@@ -1,6 +1,20 @@
-export function Start(props){
-    return(
-    <div>
-        <p>this is the starting page component</p>
-    </div>);
+export function Start(props) {
+    const { setCurrentState, setQuestion } = props;
+
+    const handleRandomClick = () => {
+        // Code to generate random question goes here
+        setQuestion("What is your biggest weakness?")
+        setCurrentState("respond")
+    }
+
+    const handleCustomClick = () => {
+        setCurrentState("respond")
+    }
+
+    return (
+        <div>
+            <p>Welcome to APP NAME!</p>
+            <button onClick={handleRandomClick}>Start Interview with random generated question</button>
+            <button onClick={handleCustomClick}>Start Interview with my own question</button>
+        </div>);
 }
