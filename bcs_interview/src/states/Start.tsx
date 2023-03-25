6755054dd7b2737ffdd561b13cf3354a009a646f
legofanclub <67127770+legofanclub@users.noreply.github.com>
2023-03-25 11:00:53 -0700

    changed css a bit

--- bcs_interview/src/states/Start.tsx
+++ bcs_interview/src/states/Start.tsx
@@ -1,6 +1,8 @@
 export function Start(props){
     return(
     <div>
+        <button>Use one of our prompts</button>
+        <button>Provide your own prompt</button> 
         <p>this is the starting page component</p>
     </div>);
 }
